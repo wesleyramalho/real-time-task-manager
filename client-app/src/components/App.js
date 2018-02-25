48ca89651a36f8500014a72ec76f57f76c1e2526
Wesley Ramalho <wesley_raa@hotmail.com>
2018-02-25 19:12:21 -0300

    Drag and drop lib installed, Cards JS and CSS done

--- client-app/src/components/App.js
+++ client-app/src/components/App.js
@@ -1,21 +1,92 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { fromJS } from 'immutable';
+import { DragDropContext } from 'react-beautiful-dnd';
 
-class App extends Component {
+import socket from '../services/socket';
+import { getApplicants } from '../services/data';
+import * as utils from '../services/utils';
+
+import List from './List';
+import css from './App.css';
+
+export default class App extends Component {
+  state = {
+    lists: fromJS([
+      {
+        id: 'contacted',
+        title: 'Contactés',
+        items: getApplicants(3),
+      },
+      {
+        id: 'meet',
+        title: 'A rencontrer',
+        items: []
+      },
+      {
+        id: 'interview',
+        title: 'Entretien',
+        items: getApplicants(1)
+      },
+    ])
+  }
+
+  componentDidMount() {
+    //move applicant to another list/index (with socket io)
+    socket.on('move-applicant', (payload) => {
+      return this.setState({
+        lists: utils.moveListItem(this.state.lists, payload),
+      })
+    })
+  }
+
+  onDragEnd = (result) => {
+    const { source, destination } = result;
+    const { lists } = this.state;
+
+    //dropped outside the list
+    if (!destination) {
+      return;
+    }
+
+    //moved to another list/index
+    const payload = {
+      fromListId: source.droppableId,
+      itemId: result.draggableId,
+      toListId: destination.droppableId,
+      toIndex: destination.index,
+    }
+
+    //send the move event to server
+    socket.emit('move-applicant', payload)
+
+    //immediately apply the changes on client
+    return this.setState({
+      lists: utils.moveListItem(lists, payload),
+    })
+  }
+
   render() {
+    const { lists } = this.state;
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <DragDropContext
+        onDragStart={this.onDragStart}
+        onDragEnd={this.onDragEnd}
+      >
+        <div className={css.layout}>
+          {
+            lists.map((list) => {
+              return (
+                <List
+                  key={list.get('id')}
+                  list={list}
+                />
+              )
+            })
+          }
+        </div>
+      </DragDropContext>
     );
   }
-}
 
-export default App;
+
+}
